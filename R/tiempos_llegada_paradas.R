93a144a69067cfec288ec4cd9f2260b5ab5a7fa7
KepaAmigoTECHFriendly <58561058+KepaAmigoTECHFriendly@users.noreply.github.com>
2022-11-24 16:57:45 +0100

    Add files via upload

--- R/tiempos_llegada_paradas.R
+++ R/tiempos_llegada_paradas.R
@@ -933,20 +933,20 @@ tiempos_llegada_paradas <- function(id_dispositivo, linea){
           }
         }
       }
-    }
 
 
-    # Comprobación si voy a escribir en el primer atributo, si no, si este tiene == "En Parada", si lleva más de 40" le asigno tiempo contrario y a tiempo contrario *1,5
-    if(flag_escritura_primer_atributo <- FALSE){
-      if(df_tiempos_actuales_contrario$value[i] == "En parada"){
-        diferencia_tiempo_en_segundos<- as.numeric(difftime(Sys.time(),as.POSIXct(as.numeric(as.character(df_tiempos_actuales_contrario$lastUpdateTs[i]))/1000, origin="1970-01-01", tz="GMT-1"),units = "secs"))
-        if(diferencia_tiempo_en_segundos >= 30){
-          flag_escritura_primer_atributo <- TRUE
-          tiempo_atributos <- paste(round(tiempos_a_marquesinas_restantes_contrario[,(i+2)]*1.5), " minutos", sep = "")
-          tiempo_atributo_tiempo_1 <- paste(tiempos_a_marquesinas_restantes_contrario[,(i+2)], " minutos", sep = "")
+      # Comprobación si voy a escribir en el primer atributo, si no, si este tiene == "En Parada", si lleva más de 40" le asigno tiempo contrario y a tiempo contrario *1,5
+      if(flag_escritura_primer_atributo <- FALSE){
+        if(df_tiempos_actuales_contrario$value[i] == "En parada"){
+          diferencia_tiempo_en_segundos<- as.numeric(difftime(Sys.time(),as.POSIXct(as.numeric(as.character(df_tiempos_actuales_contrario$lastUpdateTs[i]))/1000, origin="1970-01-01", tz="GMT-1"),units = "secs"))
+          if(diferencia_tiempo_en_segundos >= 30){
+            flag_escritura_primer_atributo <- TRUE
+            tiempo_atributos <- paste(round(tiempos_a_marquesinas_restantes_contrario[,(i+2)]*1.5), " minutos", sep = "")
+            tiempo_atributo_tiempo_1 <- paste(tiempos_a_marquesinas_restantes_contrario[,(i+2)], " minutos", sep = "")
+          }
         }
       }
-    }
+    } # Cierre else de flag_ultimo_trayecto == TRUE
 
 
     # Escritura en atributos
