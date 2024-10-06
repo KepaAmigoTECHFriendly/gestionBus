2dbed151d8ad7eee66f8754dafff623a3522c6bb
KepaAmigoTECHFriendly <58561058+KepaAmigoTECHFriendly@users.noreply.github.com>
2024-10-06 20:11:51 +0200

    Update tiempos_llegada_paradas.R

--- R/tiempos_llegada_paradas.R
+++ R/tiempos_llegada_paradas.R
@@ -607,8 +607,12 @@ tiempos_llegada_paradas <- function(id_dispositivo, linea){
             #if(id_parada_inicial == 15 | id_parada_inicial == 76){ # Bomberos o ITV
             #  sentido <- 1  # Subiendo
             #}
-            if(any(grepl(76,id_parada_inicial)) | any(grepl(15,id_parada_inicial)) | any(grepl(115,id_parada_inicial))){ # ITV, Bomberos o Sociosanitario
-              if(dif_latitudes < 0 | dif_latitudes > 0){
+            if(any(grepl(115,id_parada_inicial))){ # Sociosanitario
+              if(dif_latitudes < 0){
+                sentido <- 1  # Subiendo
+              }
+            }else if(any(grepl(76,id_parada_inicial)) | any(grepl(15,id_parada_inicial))){ # ITV o Bomberos
+              if(dif_latitudes < 0){
                 sentido <- 1  # Subiendo
               }
             }
